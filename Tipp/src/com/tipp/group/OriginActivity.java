ad76ffa631f07461b7faeb3912c5a0bd25fb71e1
Kenneth <Kenneth@Kenneth-PC>
2014-10-24 22:33:47 -0700

    Deleted fragments, added mainmenu.xml

--- Tipp/src/com/tipp/group/OriginActivity.java
+++ Tipp/src/com/tipp/group/OriginActivity.java
@@ -1,19 +1,32 @@
 package com.tipp.group;
 
 import android.os.Bundle;
+import android.support.v4.view.MenuItemCompat;
 import android.support.v7.app.ActionBarActivity;
+import android.support.v7.widget.SearchView;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 import com.tipp.R;
 
 public class OriginActivity extends ActionBarActivity {
 
 	@Override
+	  public boolean onCreateOptionsMenu(Menu menu) {
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.mainmenu, menu);
+	    MenuItem searchItem = menu.findItem(R.id.action_search);
+	    SearchView searchView = (SearchView) MenuItemCompat.getActionView(searchItem);
+	    // Configure the search info and add any event listeners
+	    return super.onCreateOptionsMenu(menu);
+
+	  } 
+	
+	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_origin);
-		if (savedInstanceState == null) {
-			getSupportFragmentManager().beginTransaction()
-					.add(R.id.menuContainer, new MenuFragment()).commit();
-		}
+
 	}
 }
